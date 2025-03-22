30521e467a9d0bcb560a2b1c37ddca5ee68be99a
CP2025 <cp2025_049@debian-BULLSEYE-live-builder-AMD64>
2025-03-21 21:58:03 -0300

    Cambié el bucle do while por for y elimine la variable i

--- tiny_mc/run.sh
+++ tiny_mc/run.sh
@@ -2,20 +2,20 @@
 
 # Valor inicial de PHOTONS
 PHOTONS=65536
-i=1
+# Saque la variable i
 
 # Archivo de salida
 OUTPUT_FILE="results.csv"
 
 # Escribir encabezado en el archivo CSV
 echo "PHOTONS,Kphotons/seg" > $OUTPUT_FILE
-
-while [ $i -le 10 ]; do
+# Iterar 10 veces
+for _ in {1..10}; do 
     echo "Running with PHOTONS=$PHOTONS"
     
     # Compilar con el nuevo valor de PHOTONS
     make clean
-    make CFLAGS="-g -std=c11 -Wall -Wextra -Werror -O3 -march=native -DPHOTONS=$PHOTONS"
+    make CFLAGS="-g -std=c11  -ffast-math -Wall -Wextra -Werror -O3 -march=native -DPHOTONS=$PHOTONS"
     
     # Ejecutar el programa y guardar la salida en una variable
     KFOTONES=$(./headless)
