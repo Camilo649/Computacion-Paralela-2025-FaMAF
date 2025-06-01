92fa6bab269b940dd0dc766074ebc3d30e6a1e9e
CP2025 <cp2025_014@debian-BULLSEYE-live-builder-AMD64>
2025-06-01 05:25:51 -0300

    Aplico el truqito de la raiz

--- tiny_mc/photon.cuh
+++ tiny_mc/photon.cuh
@@ -1,7 +1,8 @@
+#include <cuda_runtime.h>
+
 #include "xorshift32.cuh"
 
 #pragma once
-
 
 /**
 * @brief Simula la propagación de múltiples fotones en un medio dispersivo y absorbente.
@@ -20,12 +21,14 @@ __device__ __forceinline__ void photon(float* heats, float* heats_squared, Xorsh
     float weight = 1.0f;
 
     for (;;) {
-        float t = -logf(xorshift32_norm(rng));
+        float t = -__logf(xorshift32_norm(rng));
         x += t * u;
         y += t * v;
         z += t * w;
 
-        unsigned int shell = sqrtf(x * x + y * y + z * z) * shells_per_mfp;
+        float r2 = x * x + y * y + z * z;
+	float rsqrt_r = __frsqrt_rn(r2);
+	unsigned int shell = (unsigned int)(r2 * rsqrt_r * shells_per_mfp);
         if (shell >= SHELLS) shell = SHELLS - 1;
 
         float absorb = (1.0f - albedo) * weight;
@@ -40,14 +43,17 @@ __device__ __forceinline__ void photon(float* heats, float* heats_squared, Xorsh
             xi2 = 2.0f * xorshift32_norm(rng) - 1.0f;
             t = xi1 * xi1 + xi2 * xi2;
         } while (1.0f < t);
-        u = 2.0f * t - 1.0f;
-        float sqrt_term = sqrtf((1.0f - u * u) / t);
+        
+	u = 2.0f * t - 1.0f;
+        float one_minus_u2 = 1.0f - u * u;
+        float sqrt_term = one_minus_u2 * __frsqrt_rn(t);
         v = xi1 * sqrt_term;
         w = xi2 * sqrt_term;
 
+
         if (weight < 0.001f) {
             if (xorshift32_norm(rng) > 0.1f) break;
-            weight /= 0.1f;
+            weight *= 10.0f;;
         }
     }
 }
